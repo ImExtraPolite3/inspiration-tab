314a64f41d1624799103741cc0d3b41a817fa141
ExtraPolite <imextrapolite2@gmail.com>
2024-12-02 17:47:44 -0500

    create vue project and connect with extention

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="popup">
+    <h1>Vue 3 Chrome Extension</h1>
+    <button @click="handleClick">Click Me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      alert('Button clicked!')
+    },
+  },
+}
+</script>
+
+<style>
+#popup {
+  width: 300px;
+  height: 200px;
+}
+</style>
