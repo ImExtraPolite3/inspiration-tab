f23f63962b72f3a0b16366ba7d88807293a54d5d
ExtraPolite <imextrapolite2@gmail.com>
2024-12-17 23:21:49 -0500

    finish contact me section

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,7 +1,20 @@
 export default function ContactMe({ hideContact }) {
   return (
     <div id="contact-me" style={{ display: hideContact }}>
-      <h1>Hello</h1>
+      <nav>
+        <h3>Abdul</h3>
+      </nav>
+      <div>
+        <input type="text" placeholder="Email Title" />
+        <textarea
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button>send email</button>
+      </div>
     </div>
   );
 }
